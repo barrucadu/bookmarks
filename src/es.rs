8d768ee0c4e2f04df50aa7f7e5995f6623e01485
Michael Walker <mike@barrucadu.co.uk>
2024-02-16 23:35:51 +0000

    Implement search page

--- src/es.rs
+++ src/es.rs
@@ -3,6 +3,8 @@ use elasticsearch::indices::{IndicesCreateParts, IndicesDeleteParts};
 use elasticsearch::{BulkParts, ClearScrollParts, Elasticsearch, Error, ScrollParts, SearchParts};
 use serde::{Deserialize, Serialize};
 use serde_json::{json, Value};
+use std::collections::HashMap;
+use std::string::ToString;
 
 static INDEX_NAME: &str = "bookmarks";
 
@@ -144,5 +146,88 @@ pub async fn import(client: &Elasticsearch, records: Vec<Record>) -> Result<Opti
         Ok(None)
     } else {
         Ok(Some(records.len()))
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+static PAGE_SIZE: u64 = 25;
+
+pub struct SearchResult {
+    pub domains: HashMap<String, u64>,
+    pub tags: HashMap<String, u64>,
+    pub results: Vec<(Record, Option<String>)>,
+    pub total: u64,
+    pub page: u64,
+    pub pages: u64,
+}
+
+pub async fn search(
+    client: &Elasticsearch,
+    query: &Option<&str>,
+    page: u64,
+) -> Result<SearchResult, Error> {
+    let query = match query {
+        Some(q) => json!({"query_string": {"query": q, "default_field": "content"}}),
+        None => json!({"match_all": {}}),
+    };
+    let body = json!({
+        "query": query,
+        "aggs": {
+            "domains": {"terms": {"field": "domain", "size": 500}},
+            "tags": {"terms": {"field": "tag", "size": 500}},
+        },
+        "highlight": {
+            "fields": {"content": {}},
+            "fragment_size": 300,
+            "pre_tags": "<mark>",
+            "post_tags": "</mark>",
+        },
+        "from": (page - 1) * PAGE_SIZE,
+        "size": PAGE_SIZE,
+        "sort": ["_score", "title_sort"],
+    });
+
+    let response = client
+        .search(SearchParts::Index(&[INDEX_NAME]))
+        .body(body)
+        .send()
+        .await?;
+    let response_body = response.json::<Value>().await?;
+    let hits = response_body["hits"]["hits"].as_array().unwrap();
+    let results = hits.iter().map(present_hit).collect();
+    let aggs = response_body["aggregations"].clone();
+    let total = response_body["hits"]["total"]["value"].as_u64().unwrap();
+
+    Ok(SearchResult {
+        domains: agg_buckets_to_hashmap(&aggs["domains"]["buckets"]),
+        tags: agg_buckets_to_hashmap(&aggs["tags"]["buckets"]),
+        results,
+        total,
+        page,
+        pages: f64::ceil(total as f64 / PAGE_SIZE as f64) as u64,
+    })
+}
+
+fn present_hit(hit: &Value) -> (Record, Option<String>) {
+    let record = Record::deserialize(&hit["_source"]).unwrap();
+    let highlight = hit["highlight"]["content"][0]
+        .as_str()
+        .map(ToString::to_string);
+
+    (record, highlight)
+}
+
+fn agg_buckets_to_hashmap(buckets: &Value) -> HashMap<String, u64> {
+    if let Some(buckets) = buckets.as_array() {
+        let mut out = HashMap::with_capacity(buckets.len());
+        for bucket in buckets {
+            let key = bucket["key"].as_str().unwrap().to_string();
+            let doc_count = bucket["doc_count"].as_u64().unwrap();
+            out.insert(key, doc_count);
+        }
+        out
+    } else {
+        HashMap::new()
     }
 }
--- src/web/mod.rs
+++ src/web/mod.rs
@@ -2,8 +2,15 @@ use actix_web::http::StatusCode;
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder, ResponseError};
 use elasticsearch::http::transport::Transport;
 use elasticsearch::Elasticsearch;
+use lazy_static::lazy_static;
+use serde::Deserialize;
+use serde_json::{json, Value};
+use std::collections::HashMap;
 use std::net::SocketAddr;
 use std::{fmt, io};
+use tera::Tera;
+
+use crate::es;
 
 pub async fn serve(es_host: String, address: SocketAddr, allow_writes: bool) -> io::Result<()> {
     let app_state = web::Data::new(AppState {
@@ -47,6 +54,12 @@ impl AppState {
     fn elasticsearch(&self) -> Result<Elasticsearch, elasticsearch::Error> {
         Transport::single_node(&self.es_host).map(Elasticsearch::new)
     }
+
+    fn context(&self) -> tera::Context {
+        let mut context = tera::Context::new();
+        context.insert("allow_writes", &self.allow_writes);
+        context
+    }
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -56,13 +69,101 @@ async fn get_index() -> impl Responder {
     web::Redirect::to("/search").permanent()
 }
 
+///////////////////////////////////////////////////////////////////////////////
+
 #[get("/search")]
-async fn get_search(state: web::Data<AppState>) -> Result<HttpResponse, Error> {
-    match state.elasticsearch() {
-        Ok(_client) => Err(error_something_went_wrong()),
-        Err(_err) => Err(error_cannot_connect_to_search_server()),
-    }
+async fn get_search(
+    query: web::Query<FormSearchQuery>,
+    state: web::Data<AppState>,
+) -> Result<HttpResponse, Error> {
+    let q = if let Some(q) = &query.q {
+        let trimmed = q.trim();
+        if trimmed.is_empty() {
+            None
+        } else {
+            Some(trimmed)
+        }
+    } else {
+        None
+    };
+
+    let client = state
+        .elasticsearch()
+        .map_err(|_| error_cannot_connect_to_search_server())?;
+
+    let result = es::search(&client, &q, query.page.unwrap_or(1))
+        .await
+        .map_err(|_| error_cannot_connect_to_search_server())?;
+
+    let mut context = state.context();
+    context.insert("q", &q);
+    context.insert("domains", &ranked_aggregate(result.domains));
+    context.insert("tags", &ranked_aggregate(result.tags));
+    context.insert(
+        "results",
+        &result
+            .results
+            .into_iter()
+            .map(present_result)
+            .collect::<Vec<_>>(),
+    );
+    context.insert("total", &result.total);
+    context.insert("page", &result.page);
+    context.insert("pages", &result.pages);
+
+    render_html("search.html", &context)
 }
+
+#[derive(Deserialize)]
+struct FormSearchQuery {
+    q: Option<String>,
+    page: Option<u64>,
+}
+
+fn present_result((record, fragment): (es::Record, Option<String>)) -> Value {
+    let ellipsis_fragment = match fragment {
+        Some(s) => {
+            let plain: String = s.replace("<mark>", "").replace("</mark>", "");
+            match (
+                record.content.starts_with(&plain),
+                record.content.ends_with(&plain),
+            ) {
+                (true, true) => s,
+                (true, false) => s + "…",
+                (false, true) => "…".to_string() + &s,
+                (false, false) => "…".to_string() + &s + "…",
+            }
+        }
+        None => "".to_string(),
+    };
+
+    let (url, parts) = if record.title.len() == 1 {
+        (Some(record.url[0].clone()), None)
+    } else {
+        let parts = std::iter::zip(&record.title[1..], record.url)
+            .map(|(title, url)| json!({"title": title, "url": url}))
+            .collect::<Vec<_>>();
+        (None, Some(parts))
+    };
+
+    json!({
+        "title": record.title[0],
+        "url": url,
+        "domain": record.domain,
+        "tag": record.tag,
+        "parts": parts,
+        "fragment": ellipsis_fragment,
+    })
+}
+
+fn ranked_aggregate(mut agg: HashMap<String, u64>) -> Vec<(u64, String)> {
+    let mut vec: Vec<_> = agg.drain().map(|(k, v)| (v, k)).collect();
+    vec.sort();
+    vec.reverse();
+    vec
+}
+
+///////////////////////////////////////////////////////////////////////////////
 
 #[get("/new")]
 async fn get_new(state: web::Data<AppState>) -> Result<HttpResponse, Error> {
@@ -77,6 +178,31 @@ async fn post_new(state: web::Data<AppState>) -> Result<HttpResponse, Error> {
     match state.elasticsearch() {
         Ok(_client) => Err(error_something_went_wrong()),
         Err(_err) => Err(error_cannot_connect_to_search_server()),
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+lazy_static! {
+    static ref TEMPLATES: Tera = {
+        let mut tera = Tera::default();
+        let res = tera.add_raw_templates(vec![(
+            "search.html",
+            include_str!("_templates/search.html.tera"),
+        )]);
+        if let Err(error) = res {
+            panic!("could not parse templates: {error}");
+        }
+        tera
+    };
+}
+
+fn render_html(template: &str, context: &tera::Context) -> Result<HttpResponse, Error> {
+    match TEMPLATES.render(template, context) {
+        Ok(rendered) => Ok(HttpResponse::Ok()
+            .content_type(mime::TEXT_HTML_UTF_8)
+            .body(rendered)),
+        Err(_) => Err(error_something_went_wrong()),
     }
 }
 
@@ -131,7 +257,7 @@ impl Error {
         context.insert("status_code", &u16::from(self.status_code));
         context.insert("message", &self.message);
 
-        let rendered = tera::Tera::one_off(TEMPLATE_ERROR_HTML, &context, true)?;
+        let rendered = Tera::one_off(TEMPLATE_ERROR_HTML, &context, true)?;
         Ok(HttpResponse::build(self.status_code)
             .content_type(mime::TEXT_HTML_UTF_8)
             .body(rendered))
